6d2565a8e28c4300f56e31b3953b42101e6f7bc3
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-29 13:51:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # Create the ECR repository
 resource "aws_ecr_repository" "lil_repository" {
-  name = "lili-ecr-repository"
+  name = "node-express-app"
   # Additional repository configurations can be added here if needed
 }
 
