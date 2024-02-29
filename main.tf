2e4dabcb48b50c9b01a3a5d54a9cf532ef76565f
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-29 17:01:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,15 @@ provider "aws" {
   region = "us-east-1"  # Change to your desired region
 }
 
-# Create the ECR repository
-resource "aws_ecr_repository" "lil_repository" {
-  name = "node-express-app"
-  # Additional repository configurations can be added here if needed
+# Define variables
+variable "repository_name" {
+  description = "Name of the ECR repository"
+  default     = "node-express-app"  # Change with your repository name
 }
 
+# Create the ECR repository
+resource "aws_ecr_repository" "lil_ecr_repository" {
+  name = var.repository_name
+  
+}
 
